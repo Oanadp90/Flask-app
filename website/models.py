9b2368f92d579c09dd88b8b446541844fe3d1df7
Oanadp90 <oanadp90@gmail.com>
2025-01-20 14:32:13 +0000

    changes made to page 2 and 3

--- website/models.py
+++ website/models.py
@@ -1,8 +1,7 @@
 from . import db
-import datetime
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     task = db.Column(db.String(300), unique = True)
-    complete = db.Column(db.Boolean, default = False)     
+    complete = db.Column(db.Boolean, default = False)    
 
